22c8341069da4dfd6ce0dd7a712d591971f82e43
jbn8 <jbnguyen@ou.edu>
2023-02-21 20:10:45 -0600

    main method

--- ou-cs3203-assignment.cpp
+++ ou-cs3203-assignment.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 
+
+void printArray(vector<int>numbers){
+    int vect_size = numbers.size();
+
+    for (int i=0; i < vect_size; i++){
+        cout<< numbers[i]<< " ";
+    }
+}
+
 int sum(vector<int> numbers){
     int sum=0;
     int vect_size = numbers.size();
@@ -16,13 +25,54 @@ int sum(vector<int> numbers){
 }
 
 int product(vector<int> numbers){
-    int product;
+
     int vect_size = numbers.size();
+    int product =1;
 
     if (vect_size != 0){
         for (int i=0; i < vect_size; i++) {
             product = product * numbers[i];
         }
     }
+    else{
+        product=0;
+    }
     return product;
+}
+
+
+
+int main() {
+    cout<< "Hi" <<'\n';
+
+
+    vector<int> numbers; // inserts value at the last position
+    //numbers.push_back(val);
+    int val;
+    cout<< " Enter your value -1 to quit: " << '\n';
+
+    cin >> val;
+
+    while(val != -1) {
+        //int input=val;
+
+        numbers.push_back(val);
+        cout<< " Enter your value -1 to quit: " << '\n';
+        cin >> val;
+    }
+
+    cout<< "The array: " ;
+    printArray(numbers);
+    cout<<'\n';
+
+    cout<< "Sum: "<< sum(numbers)<<'\n';
+    cout<< "Product: "<<product(numbers) << '\n';
+
+
+
+
+    return 0;
+
+
+
 }
